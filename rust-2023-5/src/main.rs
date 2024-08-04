4513e36c1deb7de87b0a2faaa7ef54171b6d0b3a
jossduff <jduff360@gmail.com>
2024-08-04 12:21:02 -0400

    chunking input

--- rust-2023-5/src/main.rs
+++ rust-2023-5/src/main.rs
@@ -1,85 +1,49 @@
-use std::{fs::read_to_string, u32::MAX};
-
+use anyhow::Result;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 fn main() {
-    let input = read_to_string("input.txt").unwrap();
+    // let input = read_to_string("input.txt").unwrap();
 
-    // start with one copy of card 1 (0, 1)
-    let mut card_count: Vec<u32> = vec![1; 209];
-
-    // let _ = input
-    //     .lines()
-    //     .map(|line| obey_the_elf(line, &mut card_count));
-
-    for line in input.lines() {
-        obey_the_elf(line, &mut card_count);
-    }
-
-    let sum: u32 = card_count.iter().sum();
-    println!("{sum}");
+    // let input = input.lines().map(|line| parse_line(line, &mut card_count));
+    let input_file = "test-input.txt";
+    let _ = parse_input(input_file).unwrap();
 }
 
-fn obey_the_elf(line: &str, card_count: &mut Vec<u32>) {
-    let initial_split = line.split(":").collect::<Vec<&str>>();
+fn parse_input(input_file: &str) -> Result<()> {
+    let file = File::open(input_file).unwrap();
 
-    let card_number: u32 = if let Some(x) = initial_split.first().unwrap().split_whitespace().last()
-    {
-        x.parse().unwrap()
-    } else {
-        return;
-    };
+    let reader = BufReader::new(file);
 
-    let card_copies: u32 = *card_count.get((card_number - 1) as usize).unwrap_or(&0);
+    // Read the file into a single string
+    let content = reader
+        .lines()
+        .collect::<Result<Vec<String>, _>>()?
+        .join("\n");
 
-    let only_numbers = initial_split
-        .last()
-        .unwrap()
-        .split("|")
-        .collect::<Vec<&str>>();
+    // Split the content into chunks by double newline (empty line)
+    let chunks: Vec<&str> = content.split("\n\n").collect();
 
-    let winning_numbers = only_numbers
-        .first()
-        .unwrap()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<u32>>();
+    // Iterate through each chunk
+    for (i, chunk) in chunks.iter().enumerate() {
+        println!("Chunk {}:", i + 1);
 
-    let my_numbers = only_numbers
-        .last()
-        .unwrap()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect::<Vec<u32>>();
-
-    let num_matches = num_matches(&winning_numbers, &my_numbers);
-    println!("card num: {card_number}, card copies: {card_copies}, num_matches: {num_matches}");
-
-    // card 1 has 3 matches
-    // +(num of copies of this card) to copies of card 2, 3, and 4
-    // which is index 1, 2, and 3
-
-    for i in card_number..(card_number + num_matches) {
-        // 1, 2, 3
-        if let Some(copies) = card_count.get_mut(i as usize) {
-            *copies += card_copies;
-        } else {
-            card_count.insert(i as usize, card_copies);
+        // Iterate through each line in the chunk
+        for line in chunk.lines() {
+            println!("{}", line);
         }
 
-        let x = i + 1;
-        println!("Adding {card_copies} of card {x}");
-        // card_count[i as usize] += card_copies;
+        println!("----------"); // Separator between chunks
     }
-
-    // if num_matches == 0 {
-    //     0
-    // } else {
-    //     2_u32.pow(num_matches - 1)
-    // }
+    Ok(())
 }
 
-fn num_matches(winning_numbers: &Vec<u32>, my_numbers: &Vec<u32>) -> u32 {
-    winning_numbers
-        .iter()
-        .map(|num| if my_numbers.contains(num) { 1 } else { 0 })
-        .sum()
-}
+// fn parse_line(line: &str, card_count: &mut Vec<u32>) {
+//     let initial_split = line.split(":").collect::<Vec<&str>>();
+//
+//     let card_number: u32 = if let Some(x) = initial_split.first().unwrap().split_whitespace().last()
+//     {
+//         x.parse().unwrap()
+//     } else {
+//         return;
+//     };
+// }
