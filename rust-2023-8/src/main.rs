114c9aaff1501612bf60ab305a9f60e9e3f65b6d
jossduff <jduff360@gmail.com>
2024-08-10 17:48:28 -0400

    part 2 done

--- rust-2023-8/src/main.rs
+++ rust-2023-8/src/main.rs
@@ -2,29 +2,49 @@ mod direction;
 
 use direction::Direction;
 
-use std::{collections::HashMap, fs::read_to_string, str::FromStr};
+use num::{integer::lcm, Integer};
+use std::{collections::HashMap, fs::read_to_string, ops::Index, str::FromStr};
 
 fn main() {
     let input = read_to_string("input.txt").unwrap();
 
-    let (nodes, directions) = parse_input(input);
+    let (starting_nodes, nodes, directions) = parse_input(input);
 
-    println!("{} nodes and {} directions", nodes.len(), directions.len());
-    let steps = traverse(nodes, directions);
+    println!(
+        "{} starting_nodes, {} nodes, {} directions",
+        starting_nodes.len(),
+        nodes.len(),
+        directions.len()
+    );
+
+    let steps = traverse(starting_nodes, nodes, directions);
 
     println!("steps: {steps}");
 }
 
-struct Node {
-    left: String,
-    right: String,
+fn traverse(
+    starting_nodes: Vec<String>,
+    nodes: HashMap<String, Node>,
+    directions: Vec<Direction>,
+) -> usize {
+    let steps: Vec<usize> = starting_nodes
+        .iter()
+        .map(|n| find_steps_from(n, &nodes, &directions))
+        .collect();
+
+    steps.into_iter().reduce(|acc, s| acc.lcm(&s)).unwrap()
 }
 
-fn traverse(nodes: HashMap<String, Node>, directions: Vec<Direction>) -> usize {
-    let mut current_node = "AAA";
+fn find_steps_from(
+    starting_node: &str,
+    nodes: &HashMap<String, Node>,
+    directions: &[Direction],
+) -> usize {
+    let mut current_node = starting_node;
+
     let mut steps = 0;
     loop {
-        if current_node == "ZZZ" {
+        if current_node.ends_with("Z") {
             break;
         }
 
@@ -36,22 +56,22 @@ fn traverse(nodes: HashMap<String, Node>, directions: Vec<Direction>) -> usize {
         };
         steps += 1;
     }
-
     steps
 }
 
-fn parse_input(input: String) -> (HashMap<String, Node>, Vec<Direction>) {
+fn parse_input(input: String) -> (Vec<String>, HashMap<String, Node>, Vec<Direction>) {
     let split: Vec<&str> = input.split("\n\n").collect();
     assert!(split.len() == 2);
 
     let directions = parse_directions(split[0]);
-    let nodes = parse_nodes(split[1]);
+    let (starting_nodes, nodes) = parse_nodes(split[1]);
 
-    (nodes, directions)
+    (starting_nodes, nodes, directions)
 }
 
-fn parse_nodes(input: &str) -> HashMap<String, Node> {
+fn parse_nodes(input: &str) -> (Vec<String>, HashMap<String, Node>) {
     let mut map: HashMap<String, Node> = HashMap::new();
+    let mut starting_nodes = Vec::new();
 
     input.lines().for_each(|line| {
         // parse each line and insert it into the hash map
@@ -69,9 +89,13 @@ fn parse_nodes(input: &str) -> HashMap<String, Node> {
                 right: split[3].into(),
             },
         );
+
+        if split[0].ends_with("A") {
+            starting_nodes.push(split[0].into());
+        }
     });
 
-    map
+    (starting_nodes, map)
 }
 
 fn parse_directions(input: &str) -> Vec<Direction> {
@@ -79,4 +103,9 @@ fn parse_directions(input: &str) -> Vec<Direction> {
         .chars()
         .map(|s| Direction::from_str(&s.to_string()).unwrap())
         .collect()
+}
+
+struct Node {
+    left: String,
+    right: String,
 }
