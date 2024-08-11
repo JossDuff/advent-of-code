06ec8686c20e6bcf136d382e00b8717b42a6d9c9
jossduff <jduff360@gmail.com>
2024-08-10 20:46:29 -0400

    starting

---
+++ rust-2023-10/src/main.rs
@@ -0,0 +1,96 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+// x y plane that starts in the top left corner. [0,0] is the very top left square
+struct Field(Vec<Vec<Pipe>>);
+
+enum Pipe {
+    Vertical,
+    Horizontal,
+    NorthEastL,
+    NorthWestJ,
+    SouthWest7,
+    SouthEastF,
+    Ground,
+    Animal,
+}
+
+#[derive(Eq, PartialEq)]
+struct Coord {
+    x: usize,
+    y: usize,
+}
+
+impl Coord {
+    fn north(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y + 1,
+        }
+    }
+
+    fn south(&self) -> Self {
+        Self {
+            x: self.x,
+            y: self.y - 1,
+        }
+    }
+
+    fn east(&self) -> Self {
+        Self {
+            x: self.x + 1,
+            y: self.y,
+        }
+    }
+
+    fn west(&self) -> Self {
+        Self {
+            x: self.x - 1,
+            y: self.y,
+        }
+    }
+}
+
+impl Field {
+    fn at(&self, loc: &Coord) -> &Pipe {
+        &self.0[loc.x][loc.y]
+    }
+
+    fn next(&self, prev_loc: Coord, curr_loc: Coord) -> Coord {
+        let curr_pipe = self.at(&curr_loc);
+
+        match curr_pipe {
+            Pipe::Vertical => match prev_loc == curr_loc.south() {
+                true => curr_loc.north(),
+                false => curr_loc.south(),
+            },
+            Pipe::Horizontal => match prev_loc == curr_loc.west() {
+                true => curr_loc.east(),
+                false => curr_loc.west(),
+            },
+            Pipe::NorthEastL => match prev_loc == curr_loc.north() {
+                true => curr_loc.east(),
+                false => curr_loc.north(),
+            },
+            Pipe::NorthWestJ => match prev_loc == curr_loc.north() {
+                true => curr_loc.west(),
+                false => curr_loc.north(),
+            },
+            Pipe::SouthWest7 => match prev_loc == curr_loc.south() {
+                true => curr_loc.west(),
+                false => curr_loc.south(),
+            },
+            Pipe::SouthEastF => match prev_loc == curr_loc.south() {
+                true => curr_loc.east(),
+                false => curr_loc.south(),
+            },
+            Pipe::Ground => {
+                panic!("Current Location is the ground")
+            }
+            Pipe::Animal => {
+                panic!("Current location is the animal")
+            }
+        }
+    }
+}
